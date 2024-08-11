6fbc18f49cd83ad80b3240d8d9f2898d213b0cb1
vijaymanohar <vijay@perikala.com.my>
2024-08-11 08:37:31 +0530

    Removed cached files that are now in .gitignore

---
+++ java-core/src/test/java/com/perikala/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perikala.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
